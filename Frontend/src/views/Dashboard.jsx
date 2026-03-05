f4d1f6c34a3401abce8bba928f28022021adaebf
Alana Micheal <alanaeniola@gmail.com>
2026-03-05 16:45:18 +0100

    Done with everything

--- Frontend/src/views/Dashboard.jsx
+++ Frontend/src/views/Dashboard.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import useAxios from '../utils/useAxios'
+import { jwtDecode } from 'jwt-decode'
+import AuthContext from '../context/AuthContext'
+
 
 const Dashboard = () => {
+
+  const[res, setRes] = useState("")
+  const api = useAxios()
+  const token = localStorage.getItem("authTokens")
+
+  if (token) {
+    const decode = jwtDecode(token)
+
+    var user_id = decode.user_id
+    var username = decode.username
+    var full_name = decode.full_name
+    var image = decode.image
+    var email = decode.email
+  }
+
   return (
     <div>
-      
+      <div className='grid p-20 grid-cols-3'>
+        <div> This is my {username}</div>
+        <div> This is my {email}</div>
+       
+      </div>
     </div>
   )
 }
